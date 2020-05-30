4294ed4454552822dcbdc5ac52de0014561a2306
DenysUMCS <mars.kuty@gmail.com>
2020-05-30 18:34:46 +0200

    Zip-zap road creator

---
+++ Assets/PlayerFall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFall : MonoBehaviour
+{
+
+   private Rigidbody rb;
+
+   void Start() {
+
+      rb = GetComponent<Rigidbody> ();
+
+   }
+
+   void Update() {
+
+      RaycastHit hit;
+      if(Physics.Raycast (transform.position, Vector3.down, out hit, 5f) && hit.transform.gameObject.tag == "Ground") {
+         rb.useGravity = false;
+      } else {
+         rb.useGravity = true;
+      }
+
+   }
+}
