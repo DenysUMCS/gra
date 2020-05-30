a9fba940b6c44bb1959d3a298a059f18c24a5bf7
DenysUMCS <mars.kuty@gmail.com>
2020-05-30 17:56:05 +0200

    Add project to git

---
+++ Assets/sphereBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereBehavior : MonoBehaviour
+{
+
+   private Rigidbody rb; // Объявление новой переменной Rigidbody
+   private bool isMovingRight = true; // переменная, отражающая условное направление объекта
+   private float speed = 5f; // Скорость движения объекта
+
+   void Start() {
+      rb = GetComponent<Rigidbody> (); // Получение доступа к Rigidbody
+   }
+
+   void changeDirection() {
+      if (isMovingRight) {
+         isMovingRight = false;
+      } else {
+         isMovingRight = true;
+      }
+   }
+
+   void Update() {
+
+      if(Input.GetMouseButtonDown(0)) {
+         changeDirection();
+      }
+
+      if (isMovingRight) {
+         rb.velocity = new Vector3 (speed, 0f, 0f);
+      } else {
+         rb.velocity = new Vector3 (0f, 0f, speed);
+      }
+
+   }
+}
