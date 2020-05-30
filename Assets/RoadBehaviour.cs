5becd1b43e14024177d9eca7071b42517123a08d
DenysUMCS <mars.kuty@gmail.com>
2020-05-30 18:23:21 +0200

    Add simple road creator

---
+++ Assets/RoadBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadBehaviour : MonoBehaviour
+{
+
+   public GameObject road; // Префаб участка пути
+   private Vector3 lastpos = new Vector3 (0f,0f,0f); // Координаты установленного префаба
+
+   void Start() {
+
+      for(int i=0; i<10; i++) {
+         GameObject _platform = Instantiate (road) as GameObject;
+         _platform.transform.position = lastpos + new Vector3 (1f,0f,0f);
+         lastpos = _platform.transform.position;
+      }
+
+   }
+}
