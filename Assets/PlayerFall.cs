ee71d7cf42da7beef3014cd66a51a6d1e9b5da2c
DenysUMCS <mars.kuty@gmail.com>
2020-06-01 00:20:00 +0200

    Fall block after player move on him

--- Assets/PlayerFall.cs
+++ Assets/PlayerFall.cs
@@ -6,7 +6,8 @@ public class PlayerFall : MonoBehaviour
 {
 
    private Rigidbody rb;
-
+   private Rigidbody[]rb_arr = new Rigidbody[10];
+   private int j=0;
    	void Start() {
     	rb = GetComponent<Rigidbody> ();
    	}
@@ -15,9 +16,18 @@ public class PlayerFall : MonoBehaviour
 
       RaycastHit hit;
       if(Physics.Raycast (transform.position, Vector3.down, out hit, 5f) && hit.transform.gameObject.tag == "Ground") {
+        
          rb.useGravity = false;
+         
+        
       } else {
+      	 rb_arr[j%10]=hit.rigidbody;
          rb.useGravity = true;
+         j++;
+         rb_arr[(j+1)%10].isKinematic=false;
+         //Destroy(rb_arr[(j)%10]);
+        //hit.rigidbody.isKinematic=false;
+         //hit.rigidbody.isKinematic=false;
       }
 
    }
