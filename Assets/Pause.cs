1a3857f268814b154f117df04470bdc9395104d6
DenysUMCS <mars.kuty@gmail.com>
2020-06-16 21:28:03 +0200

    update

---
+++ Assets/Pause.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text score;
+    public static bool isPause=false;
+    void Start()
+    {
+    	isPause=!isPause;
+        score = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
